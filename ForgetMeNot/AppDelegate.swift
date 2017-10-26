6d1c282d9931a8335c17c08d323b9ce395731def
Yeontae Kim <ytk007@gmail.com>
2017-10-26 16:53:22 -0700

    Notify the user when the iBeacon is out of range

--- ForgetMeNot/AppDelegate.swift
+++ ForgetMeNot/AppDelegate.swift
@@ -22,15 +22,40 @@
 
 import UIKit
 import UserNotifications
+import CoreLocation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
+
+  let locationManager = CLLocationManager()
   
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    
+    // Request permission to send notifications
+    let center = UNUserNotificationCenter.current()
+    center.requestAuthorization(options:[.alert, .sound]) { (granted, error) in }
+    locationManager.delegate = self
     
     return true
   }
 	
+}
+
+// MARK: - CLLocationManagerDelegate
+extension AppDelegate: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+        guard region is CLBeaconRegion else { return }
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Forget Me Not"
+        content.body = "Are you forgetting something?"
+        content.sound = .default()
+        
+        let request = UNNotificationRequest(identifier: "ForgetMeNot", content: content, trigger: nil)
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+    }
+    
 }
