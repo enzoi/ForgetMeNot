2f40a5b4b7b3bd1c81ec95baeb68ea69754e0bd8
Yeontae Kim <ytk007@gmail.com>
2017-10-26 16:22:16 -0700

    Add CLLocationManagerDelegate methods

--- ForgetMeNot/ViewControllers/ItemsViewController.swift
+++ ForgetMeNot/ViewControllers/ItemsViewController.swift
@@ -141,5 +141,33 @@ extension ItemsViewController: UITableViewDelegate {
 // MARK: - CLLocationManagerDelegate
 
 extension ItemsViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, monitoringDidFailFor region: CLRegion?, withError error: Error) {
+        print("Failed monitoring region: \(error.localizedDescription)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Location manager failed: \(error.localizedDescription)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
+        
+        // Find the same beacons in the table.
+        var indexPaths = [IndexPath]()
+        for beacon in beacons {
+            for row in 0..<items.count {
+                // TODO: Determine if item is equal to ranged beacon
+            }
+        }
+        
+        // Update beacon locations of visible rows.
+        if let visibleRows = tableView.indexPathsForVisibleRows {
+            let rowsToUpdate = visibleRows.filter { indexPaths.contains($0) }
+            for row in rowsToUpdate {
+                let cell = tableView.cellForRow(at: row) as! ItemCell
+                cell.refreshLocation()
+            }
+        }
+    }
 }
 
